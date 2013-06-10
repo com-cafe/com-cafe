3e690453739356c4642e6d26d068ad4e120e7c27
Roberto Soares <roberto.tech@gmail.com>
2013-06-10 13:03:07 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Comcafe::Application.config.secret_key_base = '069ace87cb5794c50665bce7b98af5b535e6ab0db380b5696d2733131eb75318621828b79e56b77e9d0fe6952a97e2112836308d7c94b8f8372c0e8029fa7698'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Comcafe::Application.config.session_store :cookie_store, key: '_comcafe_session'
